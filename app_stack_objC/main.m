ae224a76e39f7cab9ca5500a6f04ff4164e814ef
Matthew Elbing <Matthewelbing@gmail.com>
2015-05-31 11:40:42 -0400

    Initial Commit

---
+++ app_stack_objC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  app_stack_objC
+//
+//  Created by Matthew Elbing on 5/31/15.
+//  Copyright (c) 2015 Matthew Elbing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
